b80627680053f48e87cad7fbc17fcfd56f1a305e
Liudvikas Rad <liudvikasr6@gmail.com>
2018-11-23 16:58:20 +0200

    at-1 string

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,7 +3,6 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("bybs");
-        System.out.println("bybs");
+        System.out.println("AT-1");
     }
 }
