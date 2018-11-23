6018a077a84510816c7ee91f03dda47d5e3a89d9
Liudvikas Rad <liudvikasr6@gmail.com>
2018-11-23 16:42:50 +0200

    just hello world

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,7 +3,5 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("bybs");
-        System.out.println("bybs");
     }
 }
